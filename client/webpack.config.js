6d32bfb12b2401e812895f00a7e25b3ff7e4e4f8
AlbusSeverus <ericsonhnc@gmail.com>
2023-07-10 17:58:46 -0700

    webpack.config.js updated

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,13 +17,66 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+      // `hot` option enables HMR on the server.
+      hot: 'only',
+    },
     plugins: [
-      
+      // Webpack plugin that simplifies creation of HTML files to serve your webpack bundles.
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+        favicon: "./favicon.ico",
+      }),
+      // Webpack plugin that generates our service worker file
+      new InjectManifest({
+        swSrc: './sw.js',
+        swDest: 'sw.js',
+      }),
+
+      // Webpack plugin that generates our manifest file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Babel-loader is used to transpile ES6 code to ES5.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
